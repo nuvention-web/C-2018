9de2ffd2325b000fb45b8a59d4896122d2ed9b86
Encode.X <encode.xii@gmail.com>
2018-03-10 10:37:47 -0600

    color scheme and data changes for detail page

--- src/pages/trans-detail/trans-detail.ts
+++ src/pages/trans-detail/trans-detail.ts
@@ -118,18 +118,31 @@ export class TransDetailPage {
 
           /* other stuff that requires slice's label and value */
           this._month = `${label} 2018`;
-          this.generateNewTransactions();
+          this.generateNewTransactions(label);
         }
       }
     }
   };
 
-  private _ts: any = [[
-    { name: `McDonald's`, amount: `10.74`, date: `17-02-08`, love: true },
-    { name: `Starbucks`, amount: `7.32`, date: `17-02-08`, love: false },
-    { name: `Uber 063015 SF**POOL**`, amount: `5.40`, date: `17-02-08`, love: true },
-    { name: `United Airlines`, amount: `500.00`, date: `17-02-08`, love: true }
-  ]];
+  private _ts: any = [
+    { name: `McDonald's`, amount: `10.74`, date: `2017-02-27`, love: true },
+    { name: `Starbucks`, amount: `7.32`, date: `2017-02-27`, love: false },
+    { name: `Uber 063015 SF**POOL**`, amount: `5.40`, date: `2017-02-25`, love: true },
+    { name: `United Airlines`, amount: `500.00`, date: `2017-02-23`, love: true },
+    { name: `AmazonPrime Membersh`, amount: `49.00`, date: `2017-02-23`, love: true },
+    { name: `TARGET.COM * 800-591-3869`, amount: `42.49`, date: `2017-02-22`, love: true },
+    { name: `AMAZON MKTPLACE`, amount: `27.57`, date: `2017-02-20`, love: true },
+    { name: `#03428 JEWEL EVANSTON IL`, amount: `56.20`, date: `2017-02-19`, love: true },
+    { name: `Nicor Gas NICPayment 1388019270`, amount: `50.00`, date: `2017-02-16`, love: true },
+    { name: `ZARA USA 3697 CHICAGO IL`, amount: `138.21`, date: `2017-02-12`, love: true },
+    { name: `B&H PHOTO`, amount: `298.00`, date: `2017-02-08`, love: true },
+    { name: `LITTLE TOKYO ROSEMONT`, amount: `11.15`, date: `2017-02-03`, love: true },
+    { name: `MICHAEL KORS`, amount: `141.41`, date: `2017-02-08`, love: true },
+    { name: `CALVIN KLEIN`, amount: `26.13`, date: `2017-02-06`, love: true },
+    { name: `USA*CANTEEN VENDING`, amount: `1.25`, date: `2017-02-03`, love: true },
+    { name: `NORRIS CENTER FOOD COUR`, amount: `8.02`, date: `2017-02-02`, love: true },
+    { name: `LIBRARY CAFE BERGSON`, amount: `3.85`, date: `2017-02-08`, love: true }
+  ];
 
   private _tSource = [];
 
@@ -165,6 +178,10 @@ export class TransDetailPage {
   }];
 
   private _month = `Jan 2018`;
+  private _months = {
+    "Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05", "Jun": "06",
+    "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10", "Nov": "11", "Dec": "12"
+  };
 
   constructor(
     public navCtrl: NavController,
@@ -180,18 +197,30 @@ export class TransDetailPage {
 
     this.plaidService.transactions$.subscribe(transactions => {
       this._tSource = transactions;
-      this.generateNewTransactions();
+      this.generateNewTransactions("Jan");
     });
   }
 
-  private generateNewTransactions() {
-    const l = this._tSource.length;
+  private generateNewTransactions(month) {
+    // const l = this._tSource.length;
+    const l = this._ts.length;
     this._transactions = [];
+    let numbers = [];
     for (let i = 0; i < 4 + Math.floor((Math.random() * 5)); i++) {
-      let item = this._tSource[Math.floor((Math.random() * l))];
+      // let item = this._tSource[Math.floor((Math.random() * l))];
+      let num = Math.floor((Math.random() * l));
+      let found = numbers.find(n => (n == num));
+      if (found) {
+        i--;
+        continue;
+      }
+      numbers.push(num);
+      let item = this._ts[num];
       item.love = Math.random() > 0.3;
+      item.date = `2018-${this._months[month]}-${item.date.substr(8, 2)}`;
       this._transactions.push(item);
     }
+    this._transactions.sort((a, b) => (a.date <= b.date));
   }
 
 
