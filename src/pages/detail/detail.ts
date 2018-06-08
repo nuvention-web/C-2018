2b3e76fae26d1d83add201952a34e5445a513b02
Shuqiang WEN <shuqiangwen@ShuqiangdeMacBook-Pro.local>
2018-06-08 04:13:59 -0500

    new features

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -94,7 +94,7 @@ export class DetailPage {
 
         if (activePoints.length > 0) {
           var clickedElementindex = activePoints[0]["_index"];
-          this.clearData();
+          //this.clearData();
           this.getData(clickedElementindex);
           this.showDetail = true;
         }
@@ -102,13 +102,89 @@ export class DetailPage {
       maintainAspectRatio: false,
       responsive: true
     }
-
-
   };
+    private chartOptions2 = {
+        type: `bar`,
+        data: {
+            datasets: [{
+                data: [0, 0, 0, 0, 0, 0, 0],
+                backgroundColor: [
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                    pattern.draw('dot-dash', '#ff9763'),
+                ],
+                borderColor: [
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                    'rgba(255, 151, 99,1)',
+                ],
+                borderWidth: 1
+            },
+                {
+                    data: [0, 0, 0, 0, 0, 0, 0],
+                    backgroundColor: [
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                        'rgba(17, 178, 69, 0.2)',
+                    ],
+                    borderColor: [
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                        'rgba(17, 178, 69,1)',
+                    ],
+                    borderWidth: 1
+                }],
+            labels: ['Food and Drink', 'Restaurants', 'Coffee Shop', 'Chase QuickPay', 'Shops', 'Service', 'Other']
+        },
+        options: {
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }],
+                xAxes: [{
+                    ticks: {
+                        fontSize: 7
+                    }
+                }],
+            },
+            onClick: (evt) => {
+                var activePoints = this.chart2.getElementsAtEvent(evt);
+
+                if (activePoints.length > 0) {
+                    var clickedElementindex = activePoints[0]["_index"];
+                    this.getData(clickedElementindex);
+                    this.showDetail2 = true;
+                }
+            },
+            maintainAspectRatio: false,
+            responsive: true
+        }
+    };
 
   demoItem = { name: `Domino's Pizza`, amount: 37.87, loved: false };
   showDetail = false;
-
+  showDetail2 = false;
   constructor(
     private zone: NgZone,
     public navCtrl: NavController,
@@ -127,13 +203,12 @@ export class DetailPage {
     this.m.set('Chase QuickPay', 3);
     this.m.set('Shops', 4);
     this.m.set('Service', 5);
-    this.m.set('Transfer', 6);
-    this.getData(new Date().getDay());
 
-    // this.chart = new Chart(`chart-canvas`, this.chartOptions);
+    this.chart = new Chart(`chart-canvas`, this.chartOptions);
     // this.chart.update();
-    // this.chart2 = new Chart(`chart-canvas-2`, this.chartOptions);
+    this.chart2 = new Chart(`chart-canvas-2`, this.chartOptions2);
     // this.chart2.update();
+      this.getData(new Date().getDay());
   }
 
   private _access_token: string = this.navParams.get("accessToken");
@@ -141,15 +216,19 @@ export class DetailPage {
   private month = new Date().getMonth();
   private year = new Date().getFullYear();
   private _transactions: any = [];
+  private _transactions2: any = [];
   private from;
   private to;
   clearData() {
     for (var z3 = 0; z3 < this.chartOptions.data.datasets[0].data.length; z3++) {
       this.chartOptions.data.datasets[0].data[z3] = 0;
       this.chartOptions.data.datasets[1].data[z3] = 0;
+      this.chartOptions2.data.datasets[0].data[z3] = 0;
+      this.chartOptions2.data.datasets[1].data[z3] = 0;
       console.log(`wen test 133 ${this.chartOptions.data.datasets[0].data[z3]}`);
     }
     this._transactions = [];
+    this._transactions2 = [];
   }
 
   getData(clickedElementindex) {
@@ -164,6 +243,7 @@ export class DetailPage {
       this.clearData();
       this.plaidService.getTransactionRecords(this._userId, this.from, this.to).then(r => {
         this._transactions.length = 0;
+        this._transactions2.length = 0;
         let result = r.filter(t => t.flagged == true || t.flagged == null);
         this.clearData();
         result.forEach(t => {
@@ -173,10 +253,25 @@ export class DetailPage {
             var day = new Date(target["date"]).getDay();
             if (target["loved"] == true) {
               this.chartOptions.data.datasets[1].data[day] += target["amount"];
+                for(var z6 = 0; z6 < target["category"].length; z6++) {
+                    var temp = 6;
+                    if(this.m.has(target["category"][z6]))
+                        temp = this.m.get(target["category"][z6]);
+                    this.chartOptions2.data.datasets[1].data[temp] += target["amount"];
+                    if(temp == clickedElementindex)
+                        this._transactions2.push(target);
+                }
             }
             else {
               this.chartOptions.data.datasets[0].data[day] += target["amount"];
-
+                for(var z6 = 0; z6 < target["category"].length; z6++) {
+                    var temp = 6;
+                    if(this.m.has(target["category"][z6]))
+                        temp = this.m.get(target["category"][z6]);
+                    this.chartOptions2.data.datasets[0].data[temp] += target["amount"];
+                    if(temp == clickedElementindex)
+                        this._transactions2.push(target);
+                }
             }
             if (clickedElementindex == day) {
               this._transactions.push(target);
@@ -184,8 +279,8 @@ export class DetailPage {
           }
         });
         this.roundAll();
-        this.chart = new Chart(`chart-canvas`, this.chartOptions);
         this.chart.update();
+        this.chart2.update();
       })
     });
 
@@ -193,7 +288,6 @@ export class DetailPage {
 
   getData2() {
     var clickedElementindex = new Date().getDay();
-    this.clearData();
     this.getData(clickedElementindex);
   }
 
@@ -205,11 +299,14 @@ export class DetailPage {
     for (var z3 = 0; z3 < this.chartOptions.data.datasets[0].data.length; z3++) {
       this.chartOptions.data.datasets[0].data[z3] = this.round(this.chartOptions.data.datasets[0].data[z3]);
       this.chartOptions.data.datasets[1].data[z3] = this.round(this.chartOptions.data.datasets[1].data[z3]);
+      this.chartOptions2.data.datasets[0].data[z3] = this.round(this.chartOptions2.data.datasets[0].data[z3]);
+      this.chartOptions2.data.datasets[1].data[z3] = this.round(this.chartOptions2.data.datasets[1].data[z3]);
     }
 
   }
 
   closeWindow() {
     this.showDetail = false;
+    this.showDetail2 = false;
   }
 }
